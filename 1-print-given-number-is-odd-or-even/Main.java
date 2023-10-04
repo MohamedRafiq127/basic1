7d8f6a10489a67a4801ffa372c86c8f949346b25
MohamedRafiq127 <98868169+MohamedRafiq127@users.noreply.github.com>
2023-10-04 11:50:07 +0530

    Update Main.java

--- 1-print-given-number-is-odd-or-even/Main.java
+++ 1-print-given-number-is-odd-or-even/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter a number");
+	System.out.println("Enter a number ");
 	int number=sc.nextInt();
 	if(number%2==0){
 	    System.out.println("Given number is even " + number);
